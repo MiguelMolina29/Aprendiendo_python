febcb593d8cfac9aa1f523d77ffbd62faa45ee70
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-08-19 21:38:51 -0500

    Primer commit

---
+++ 01-variable.py
@@ -0,0 +1,15 @@
+# Una variable es un elemento de un lenguaje de programacion que tiene asignado un valor determinado
+# Para determinar una variable en python, se le debe proporcionar un nombre y su valor utilizando el simbolo '='
+
+var = "Hola mundo"
+# print(var)
+
+var = "Adios mundo"
+# print( var )
+
+
+# Asignacion Multiple
+# Otra cosa que podemos hacer con las variables en Python es asignar una variable a otra variable diferente
+
+var2 = var
+print( var2 )
