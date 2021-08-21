87d00facbd3a9fd30371ee0c6379f5127010a3ab
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-08-20 20:25:36 -0500

    String

--- 01-variable.py
+++ 01-variable.py
@@ -12,4 +12,14 @@ var = "Adios mundo"
 # Otra cosa que podemos hacer con las variables en Python es asignar una variable a otra variable diferente
 
 var2 = var
-print( var2 )
+# print( var2 )
+
+
+# Sintaxis de las variables en Python 
+# Pueden tener cualquier longitud y pueden consistir en letras mayusculas y minusculas, digitos y caracter de subrayado _
+
+_var = "Esto es un valor"
+print( _var )
+
+# El primer caracter de un nombre de variable no puede ser un digito
+# El nombre de las variables en Python es sensible a mayusculas y minusculas 
---
+++ 02-strings.py
@@ -0,0 +1,17 @@
+# Strings, Indexing, Slicing y Stride parte 1
+
+# Un string se corresponde con un conjunto de caracteres que forman una cadena de texto 
+# Se pueden definir como " "  o ' '
+
+var = "String con comillas dobles"
+var2 = "String con comillas sencillas"
+
+# Indexacion 
+# En muchos tipos de datos en Python se pueden acceder a elementos individuales de un conjunto ordenado de datos
+# directamente mediante un indice numerico o un valor. Este proceso se llama indexacion 
+
+# En Python las cadenas son secuencias ordenadas de caracteres y por lo tanto pueden ser indexadas de esta manera. Se
+# pueden acceder a los caracteres individuales de una cadena especificando el nombre dela cadena seguido de un numero
+# entre corchetes [] => Son como los arreglos en js
+# El primer caracter de la cadena tiene el indice 0 y el siguiente 1 y asi  sucesivamente
+
