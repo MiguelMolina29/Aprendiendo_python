d180d46a64f0b92b73aac917d266f599a1807cfd
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-08-22 20:49:40 -0500

    string-listo

--- 02-strings.py
+++ 02-strings.py
@@ -7,6 +7,7 @@ var = "String con comillas dobles"
 var2 = "String con comillas sencillas"
 
 # Indexacion 
+
 # En muchos tipos de datos en Python se pueden acceder a elementos individuales de un conjunto ordenado de datos
 # directamente mediante un indice numerico o un valor. Este proceso se llama indexacion 
 
@@ -15,3 +16,34 @@ var2 = "String con comillas sencillas"
 # entre corchetes [] => Son como los arreglos en js
 # El primer caracter de la cadena tiene el indice 0 y el siguiente 1 y asi  sucesivamente
 
+nombre = "Antonio"
+#print(nombre[2]) # Tambien se puede utilizar numeros negativos
+
+#Slicing
+
+# Python tambien permite una sintaxis especifica de indexacion que extrae subcadenas de una cadena de texto, a esto se le denomina slicing
+
+nombre = "Antonio Molina"
+# print( nombre[0:8] )
+# Son maneras de hacer rangos
+
+# Stride 
+
+# El stride es otra variante mas del slicing. Si te añade un : adicional y un tercer indice, se designa un stride, que indica 
+# cuantos caracteres saltar hasta obtener el siguiente caracter
+
+nombre = "Miguel Antonio"
+# print( nombre[0:8:3] )
+
+# Modificacion de strings
+
+# Un string es un tipo de dato que python considera inmutable, esto quiere decir que no podemos modificar una parte de un string asociada a una variable
+
+# nombre = "Antonio"
+# print( nombre[3] )
+# nombre[3] = "e"
+# print(nombre)
+
+# String de multiples lineas
+
+# Para hacerlo corto => "\n" con esto se hace el tipico salto de linea
